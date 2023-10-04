454967ffa3f30aad14817dce4eda1a9ade728c54
Ricky JW <rickywid@hotmail.com>
2023-10-04 00:02:28 -0400

    add comments

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var movement_data:PlayerMovementData
 
+# default gravity settings defined in Project Settings
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var air_jump = false
 var just_wall_jumped = false
@@ -10,7 +11,15 @@ var just_wall_jumped = false
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var starting_position = global_position
 
+# Games run in a loop. Each frame, you need to update the state of your game world before drawing it on screen. 
+# Between each frame, the _physics_process() will be called at a fixed rate (60fps by default).
+#
+# The game engine will try to run at the highest FPS (frames per second). 
+# The function's delta parameter is the time elapsed in seconds since the previous call to _physics_process().
+#
+# https://docs.godotengine.org/en/stable/tutorials/scripting/idle_and_physics_processing.html#doc-idle-and-physics-processing
 func _physics_process(delta):
+
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	
 	apply_gravity(delta)
@@ -29,6 +38,8 @@ func _physics_process(delta):
 	if just_left_ledge:
 		coyote_jump_timer.start()
 		
+	# If spacebar key is pressed, load the FasterMovementData property values (make character
+	# move faster)
 	if Input.is_action_just_pressed("ui_accept"):
 		movement_data = load("res://FasterMovementData.tres")
 	
@@ -37,6 +48,7 @@ func apply_gravity(delta):
 			velocity.y += gravity * movement_data.gravity_scale * delta
 			
 func handle_wall_jump():
+	# is_on_wall_only() prevents character from wall jumping while also standing on the ground.
 	if not is_on_wall_only(): return
 	var wall_normal = get_wall_normal()
 	if Input.is_action_just_pressed("ui_up"):
@@ -44,15 +56,22 @@ func handle_wall_jump():
 		velocity.y = movement_data.jump_velocity
 		just_wall_jumped = true	
 
+
+# character should only be able to perform double jump (air jump) once when jumping from the ground 
+# in order to prevent user from being able to air jump indefinitely.
 func handle_jump():
 	if is_on_floor(): air_jump = true
+	# Perform jump if character is on floor or if user has jumped within the CoyoteJumpTimer's Wait Time
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		# Perform normal jump (long key press)
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = movement_data.jump_velocity   
 	elif not is_on_floor():
+		# Perform short jump (quick key press)
 		if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
 		if Input.is_action_just_pressed("ui_up") and air_jump and not just_wall_jumped:
+			# perform double jump (air jump) at 80% jump velocity
 			velocity.y = movement_data.jump_velocity * 0.8 
 			air_jump = false
 	
